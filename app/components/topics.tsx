efff5789e9e7b27f314b67415ac448679a677696
Bruno Pena <brunopena454@gmail.com>
2026-02-01 21:35:56 -0300

    Edição de style do topics

--- app/components/topics.tsx
+++ app/components/topics.tsx
@@ -4,17 +4,18 @@ import { Plus, Percent, SquarePen } from 'lucide-react'
 
 export function Topics() {
   const boxStyle =
-    'relative flex flex-col items-center justify-center gap-6 ' +
-    'w-[calc(80%/3)] min-h-[360px] ' +
-    'bg-white/90 backdrop-blur-sm rounded-3xl p-6 ' +
-    'shadow-lg border border-gray-700/20'
+  `relative flex flex-col items-center text-center 
+  w-[calc(80%/3)] h-[40vh]
+  bg-white/90 backdrop-blur-sm rounded-3xl p-8
+  shadow-lg border border-gray-700/20
+  justify-center gap-6`
 
   const topicImageStyle =
-    'absolute -top-10 w-24 h-24 rounded-full ' +
-    'bg-white p-6 text-gray-800/80 shadow-xl'
+    `absolute -top-10 w-24 h-24 rounded-full 
+    bg-white p-6 text-gray-800/80 shadow-xl overflow-visible`
 
   const buttonStyle =
-    'mt-auto px-6 py-3 w-[70%] flex justify-center items-center ' +
+    'mt-auto px-6 p-3 w-[70%] flex justify-center items-center ' +
     'bg-blue-600 text-white rounded-full hover:bg-blue-700 transition'
 
   const topics = [
@@ -47,7 +48,7 @@ export function Topics() {
         pointer-events-none
       "
     >
-      <div className="flex gap-8 w-full max-w-7xl justify-center px-6">
+      <div className="flex gap-8 w-full max-w-7xl justify-center px-6 pointer-events-auto">
         {topics.map((topic, index) => (
           <div key={index} className={boxStyle}>
             {topic.image}
